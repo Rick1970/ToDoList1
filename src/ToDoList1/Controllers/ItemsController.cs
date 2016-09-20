494d4663083c9e44d20f99ebef25b04cbbb6858a
rthor <rthor@LAPTOP-343NT6OO>
2016-09-20 10:34:57 -0700

    Getting list to work.

---
+++ src/ToDoList1/Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList1.Models;
+using System.Linq;
+
+
+
+
+namespace ToDoList1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoList1Context db = new ToDoList1Context();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+    }
+}
---
+++ src/ToDoList1/Models/ToDoList1Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList1.Models
+{
+    public class ToDoList1Context : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList1;integrated security=True");
+        }
+    }
+}
