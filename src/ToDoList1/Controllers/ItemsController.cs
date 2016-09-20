5463d447fa1d9fe076039327af2961bac18ac9b7
rthor <rthor@LAPTOP-343NT6OO>
2016-09-20 12:36:11 -0700

    add create, edit, and delete functionality.

--- src/ToDoList1/Controllers/ItemsController.cs
+++ src/ToDoList1/Controllers/ItemsController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ToDoList1.Models;
 using System.Linq;
-
-
-
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList1.Controllers
 {
@@ -19,6 +17,41 @@ namespace ToDoList1.Controllers
             var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
             return View(thisItem);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-
 }
