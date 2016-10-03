2078f167a034f52cb309b5ebdff394a7119363bf
rthor <rthor@LAPTOP-343NT6OO>
2016-10-03 15:10:10 -0700

    tests added; first test passsed.

---
+++ ToDoList1.Tests/ModelTests/ItemTest.cs
@@ -0,0 +1,22 @@
+﻿using ToDoList1.Models;
+using Xunit;
+
+namespace ToDoList1.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Item();
+            item.Description = "Wash the dog";
+
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+    }
+}
